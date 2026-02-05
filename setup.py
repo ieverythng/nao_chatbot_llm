6e4269dc39842b675122bc0fa3fcd891bc0cb5d1
Séverin Lemaignan <severin@guakamole.org>
2026-02-05 17:02:32 +0100

    initial scaffold, generated with rpk

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+from glob import glob
+
+NAME = "chatbot_llm"
+
+setup(
+    name=NAME,
+    version="0.1.0",
+    license="Apache-2.0",
+    description="A ollama-based LLM bridge for robotic applications",
+    author="todo",
+    author_email="todo@todo.todo",
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + NAME, ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['res/' + NAME]),
+        ('share/' + NAME + '/launch', glob('launch/*.launch.py')),
+        ('share/ament_index/resource_index/pal_system_module',
+         ['module/' + NAME]),
+        ('share/' + NAME + '/module', ['module/' + NAME + '_module.yaml']),
+        ('share/ament_index/resource_index/pal_configuration.' + NAME,
+            ['config/' + NAME]),
+        ('share/' + NAME + '/config', ['config/00-defaults.yml']),
+    ],
+    tests_require=['pytest'],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'start_node = ' + NAME + '.start_node:main'
+        ],
+    },
+)
